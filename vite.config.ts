5c79d35c8db74ca1304d4252a3cbcea9ff569818
FernandaSPfiz <fspfiz@minha.fag.edu.br>
2025-06-25 20:16:53 -0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '': {
+      '/api': {
         target: 'https://enviar-mail.azurewebsites.net/',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        rewrite: (path) => path.replace(/^\//, '')
       }
     }
   }
